b851a4d26e58b1b5bab144faab9561bada2f7497
Olivier Combe <olivier.combe@gmail.com>
2019-01-09 14:51:37 +0100

    fix: ignore team requested reviews
    The GraphQL request didn't return all of the informations required to distinguish users & teams because of OAuth App access restrictions on the Angular repository.
    I switched back to rest requests which now contain all of the information necessary (that was not the case a few months ago).
    We now only count the user request reviews instead of users + teams.

--- functions/src/typings.ts
+++ functions/src/typings.ts
@@ -18,3 +18,20 @@ export const enum REVIEW_STATE {
   Commented = 'COMMENTED',
   Dismissed = 'DISMISSED'
 }
+
+export const enum AUTHOR_ASSOCIATION {
+  // Author has been invited to collaborate on the repository.
+  Collaborator = 'COLLABORATOR',
+  // Author has previously committed to the repository.
+  Contributor = 'CONTRIBUTOR',
+  // Author has not previously committed to GitHub.
+  FirstTimer = 'FIRST_TIMER',
+  // Author has not previously committed to the repository.
+  FirstTimeContributor = 'FIRST_TIME_CONTRIBUTOR',
+  // Author is a member of the organization that owns the repository.
+  Member = 'MEMBER',
+  // Author has no association with the repository.
+  None = 'NONE',
+  // Author is the owner of the repository.
+  Owner = 'OWNER'
+}
